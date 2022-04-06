a4ca21aec5d09ca5930d61e0b53160a3e8cf69cc
cameron98barnes <cameron.barnes.98@mail.ru>
2022-04-06 16:36:19 +0800

    deploy web : V0.01:update

---
+++ assets/welcome.html.944fc29e.js
@@ -0,0 +1,15 @@
+const e={key:"v-1abeb830",path:"/welcome.html",title:"Welcome to Vault.Cab",lang:"en-US",frontmatter:{index:1,title:"Welcome to Vault.Cab",author:"Vault.Cab",date:"2022-03-28T00:00:00.000Z",category:["Guide"],tag:["Welcome"],sticky:!0,star:!0,summary:`Vault Cab : Asset Treasury Vault Cab.
+You can support us at:
+
+gitcoin.co :
+dorahacks.io :
+devpost.com :
+
+`,head:[["meta",{property:"og:url",content:"https://vault.cab/welcome.html"}],["meta",{property:"og:title",content:"Welcome to Vault.Cab"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{property:"article:author",content:"Vault.Cab"}],["meta",{property:"article:tag",content:"Welcome"}],["meta",{property:"article:published_time",content:"2022-03-28T00:00:00.000Z"}]]},excerpt:`<p>Vault Cab : Asset Treasury Vault Cab.</p>
+<p>You can support us at:</p>
+<ol>
+<li>gitcoin.co :</li>
+<li>dorahacks.io :</li>
+<li>devpost.com :</li>
+</ol>
+`,headers:[{level:2,title:"Vault Cab",slug:"vault-cab",children:[]},{level:2,title:"Syber Connect",slug:"syber-connect",children:[]},{level:2,title:"Why VaultCab?",slug:"why-vaultcab",children:[]},{level:2,title:"Tech Stack to Build Vault Cab",slug:"tech-stack-to-build-vault-cab",children:[{level:3,title:"Identity Management",slug:"identity-management",children:[]},{level:3,title:"Account System",slug:"account-system",children:[]},{level:3,title:"Decentralized Networks",slug:"decentralized-networks",children:[]}]}],git:{createdTime:1649233758e3,updatedTime:1649233758e3,contributors:[{name:"cameron98barnes",email:"cameron.barnes.98@mail.ru",commits:1}]},readingTime:{minutes:.45,words:134},filePathRelative:"welcome.md"};export{e as data};
